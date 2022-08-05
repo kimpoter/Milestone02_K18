53bc77b97cd01e91f4ac5c449b14c960efcc57ff
debbyalmadea <almadeaputri@gmail.com>
2022-08-06 01:25:13 +0700

    add router for sign in and sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 import ScrollToTop from "./components/ScrollToTop";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   const [ dropdownState, setDropdownState ] = useState(false)
@@ -19,6 +21,8 @@ function App() {
       <div className="pt-[100px]">
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/signin" element={<SignInPage />} />
+          <Route exact path="/signup" element={<SignUpPage />} />
         </Routes>
       </div>
     </div>
