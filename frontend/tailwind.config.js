7edadba62e4264024d26b3626660d7dee4155a32
debbyalmadea <almadeaputri@gmail.com>
2022-08-06 00:49:14 +0700

    add font family and colors to tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Prompt'],
+      },
+      colors: {
+        'primary': '#1C2540',
+        'secondary': '#243E73',
+        'greyscale': '#EFEFEF',
+      },
+    },
   },
   plugins: [],
 };
