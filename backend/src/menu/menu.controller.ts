74de17927adf452e3aad70deb7c82c65ff1efa61
Altair1618 <farhannabilsuryono3@gmail.com>
2022-08-15 17:44:13 +0700

    update-menu

--- backend/src/menu/menu.controller.ts
+++ backend/src/menu/menu.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Public } from 'src/common/decorators';
+import { CreateMenuDto } from './dto';
 import { MenuService } from './menu.service';
 
 @Controller('menu')
@@ -7,8 +8,26 @@ export class MenuController {
     constructor(private menuService: MenuService) { }
 
     @Public()
-    @Get()
-    getTempatMakanId() {
-        return this.menuService.getTempatMakanId()
+    @Get('/:id')
+    getMenu(@Param('id') id : number) {
+        return this.menuService.getMenu(+id)
+    }
+
+    @Public()
+    @Post('/:id')
+    createMenu(@Param('id') id : number, @Body() dto : CreateMenuDto) {
+        return this.menuService.createMenu(dto, +id)
+    }
+
+    @Public()
+    @Put('/:id')
+    updateMenu(@Param('id') id : number, @Body() dto : CreateMenuDto) {
+        return this.menuService.updateMenu(dto, +id)
+    }
+
+    @Public()
+    @Delete('/:id')
+    deleteMenu(@Param('id') id : number) {
+        return this.menuService.deleteMenu(+id)
     }
 }
