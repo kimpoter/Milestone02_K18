d391e58197172f5f536ddae05027e0a605085480
Fikri Naufal Hamdi <16521293@mahasiswa.itb.ac.id>
2022-08-15 23:51:52 +0700

    Implement onSubmit function in SignUpPage.js

--- frontend/src/pages/SignUpPage.js
+++ frontend/src/pages/SignUpPage.js
@@ -1,26 +1,46 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
 function SignUpPage() {
+
+    const usernameRef = React.useRef(null);
+    const emailRef = React.useRef(null);
+    const passwordRef = React.useRef(null);
+    const confirmPWRef = React.useRef(null);
+
+    function onSubmit(){
+
+        const username = usernameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPW = confirmPWRef.current.value;
+
+        console.log("Username \t\t\t: ", username);
+        console.log("Email \t\t\t\t: ", email);
+        console.log("Password \t\t\t: ", password);
+        console.log("Confirm Password \t: ", confirmPW);
+    }
+
     return (
         <div className="flex justify-center items-center h-[calc(100vh-104px)]">
             <div className="card">
                 <h1 className="sm:text-3xl text-2xl font-semibold text-center mb-12">Sign Up</h1>
-                <form className="sm:text-lg text-base">
+                <form className="sm:text-lg text-base" onSubmit={onSubmit}>
                     <div className="flex flex-col mb-4">
                         <label>Username</label>
-                        <input type="text" required className="form-input" />
+                        <input type="text" required className="form-input" ref={usernameRef}/>
                     </div>
                     <div className="flex flex-col mb-4">
                         <label>Email</label>
-                        <input type="email" required className="form-input" />
+                        <input type="email" required className="form-input" ref={emailRef}/>
                     </div>
                     <div className="flex flex-col mb-4">
                         <label>Password</label>
-                        <input type="password" required className="form-input" />
+                        <input type="password" required className="form-input" ref={passwordRef}/>
                     </div>
                     <div className="flex flex-col mb-4">
                         <label>Confirm Password</label>
-                        <input type="password" required className="form-input" />
+                        <input type="password" required className="form-input" ref={confirmPWRef}/>
                     </div>
                     <p className="text-sm opacity-50">Sudah punya akun? <Link to='/signin' className="hover:underline text-sm">Sign in</Link></p>
                     <div className="w-full flex justify-center mt-12">
