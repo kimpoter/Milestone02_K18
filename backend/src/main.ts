bf9773005c02bf6219a782b0dcb35ed5629f1efd
Hari Sudewa <83929596+sozyGithub@users.noreply.github.com>
2022-08-16 13:50:32 +0800

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,6 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://itbfood.herokuapp.com',
     credentials: true
   })
   app.useGlobalPipes(new ValidationPipe());
