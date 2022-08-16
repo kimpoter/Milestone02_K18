af96b258b49f00d4a2cd14d3b7d7612c887c4f50
debbyalmadea <almadeaputri@gmail.com>
2022-08-16 13:19:16 +0700

    test axios for auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./AuthContext";
 import { CampusContextProvider } from "./CampusContext";
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <CampusContextProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </CampusContextProvider>
+    <CookiesProvider>
+      <CampusContextProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </CampusContextProvider>
+    </CookiesProvider>
   </BrowserRouter>
 );
