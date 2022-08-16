98283fe61b17d58593b88a9755bf21810c0194a8
Hari Sudewa <83929596+sozyGithub@users.noreply.github.com>
2022-08-16 14:01:03 +0800

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: process.env.CORS_URL || 'http://localhost:3000',
     credentials: true
   })
   app.useGlobalPipes(new ValidationPipe());
